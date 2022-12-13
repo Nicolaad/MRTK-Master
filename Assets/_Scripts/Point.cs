43531418130f1072dd170671b6e75be3a7f24609
Nicolai <dalaaker.nicolai@gmail.com>
2022-12-13 11:16:03 +0100

    created baseclasses

---
+++ Assets/_Scripts/Point.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Point : MonoBehaviour
+{
+   private Vector3 Position{get; set;}
+   [SerializeField] private static GameObject pointPrefab;
+
+}
---
+++ Assets/_Scripts/Shape.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    private Point[] points;
+    private GameObject shapePrefab;
+
+    
+}
